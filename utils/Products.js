2078c0ba25b1fcc8d3ac4821b025a7dfe2f10512
Emujede Hilary <104493382+dev-hills@users.noreply.github.com>
2025-03-14 14:53:34 +0100

    image fixing

--- utils/Products.js
+++ utils/Products.js
@@ -5,26 +5,26 @@ export const productData = {
       title: 'Ammonium Sulphate',
       description:
         'A nitrogen-rich fertilizer that promotes plant growth and soil acidity regulation.',
-      image: '../public/productImage1.JPG',
+      image: '/productImage1.JPG',
     },
     {
       category: 'Agro Chemicals',
       title: 'Glyphosate',
       description: 'A broad-spectrum herbicide used to control weeds and unwanted vegetation.',
-      image: '../public/productImage2.JPG',
+      image: '/productImage2.JPG',
     },
     {
       category: 'Agro Chemicals',
       title: 'Paraquat',
       description: 'A fast-acting, non-selective herbicide effective for weed and grass control.',
-      image: '../public/productImage3.JPG',
+      image: '/productImage3.JPG',
     },
     {
       category: 'Agro Chemicals',
       title: 'Potassium Nitrate',
       description:
         'A fertilizer that provides essential potassium and nitrogen for plant health and yield improvement.',
-      image: '../public/productImage4.JPG',
+      image: '/productImage4.JPG',
     },
   ],
   plastics: [
@@ -33,33 +33,33 @@ export const productData = {
       title: 'Calcium Carbonate Micronized',
       description:
         'A fine mineral powder used as a filler to enhance plastic strength and durability.',
-      image: '../public/productImage5.JPG',
+      image: '/productImage5.JPG',
     },
     {
       category: 'Plastics',
       title: 'DOP (Dioctyl Phthalate)',
       description: 'A plasticizer that increases flexibility and softness in plastic products.',
-      image: '../public/productImage6.JPG',
+      image: '/productImage6.JPG',
     },
     {
       category: 'Plastics',
       title: 'Organic Pigments',
       description: 'Colorants used to provide vibrant and stable colors in plastic manufacturing.',
-      image: '../public/productImage7.JPG',
+      image: '/productImage7.JPG',
     },
     {
       category: 'Plastics',
       title: 'Stearic Acid',
       description:
         'A lubricant and stabilizer that improves plastic processing and prevents brittleness.',
-      image: '../public/productImage8.JPG',
+      image: '/productImage8.JPG',
     },
     {
       category: 'Plastics',
       title: 'Titanium Dioxide',
       description:
         'A white pigment that enhances brightness, opacity, and UV resistance in plastics.',
-      image: '../public/productImage5.JPG',
+      image: '/productImage5.JPG',
     },
   ],
   food: [
@@ -67,61 +67,61 @@ export const productData = {
       category: 'Food',
       title: 'Ammonium Bicarbonate',
       description: 'A leavening agent used in baking to make dough rise',
-      image: '../public/productImage15.JPG',
+      image: '/productImage15.JPG',
     },
     {
       category: 'Food',
       title: 'Ascorbic Acid',
       description: 'Also known as Vitamin C, used as an antioxidant and preservative.',
-      image: '../public/productImage16.JPG',
+      image: '/productImage16.JPG',
     },
     {
       category: 'Food',
       title: 'Aspartame',
       description: 'A low-calorie artificial sweetener used in sugar-free products.',
-      image: '../public/productImage17.JPG',
+      image: '/productImage17.JPG',
     },
     {
       category: 'Food',
       title: 'Calcium Carbide',
       description: 'A chemical used to ripen fruits artificially.',
-      image: '../public/productImage18.JPG',
+      image: '/productImage18.JPG',
     },
     {
       category: 'Food',
       title: 'Corn Starch',
       description: 'A thickening agent used in soups, sauces, and desserts.',
-      image: '../public/productImage19.JPG',
+      image: '/productImage19.JPG',
     },
     {
       category: 'Food',
       title: 'Monosodium Glutamate (MSG)',
       description: 'A flavor enhancer used in savory dishes.',
-      image: '../public/productImage20.JPG',
+      image: '/productImage20.JPG',
     },
     {
       category: 'Food',
       title: 'Monopropylene Glycol',
       description: 'A food additive used as a humectant, stabilizer, and carrier for flavors.',
-      image: '../public/productImage21.JPG',
+      image: '/productImage21.JPG',
     },
     {
       category: 'Food',
       title: 'Methylene Chloride',
       description: 'A solvent used in caffeine extraction for decaffeinated coffee.',
-      image: '../public/productImage22.JPG',
+      image: '/productImage22.JPG',
     },
     {
       category: 'Food',
       title: 'Menthol Crystal',
       description: 'A cooling agent used in confectionery and breath fresheners.',
-      image: '../public/productImage23.JPG',
+      image: '/productImage23.JPG',
     },
     {
       category: 'Food',
       title: 'Malto Dextrine',
       description: 'A carbohydrate derived from starch, used as a thickener and energy source.',
-      image: '../public/productImage24.JPG',
+      image: '/productImage24.JPG',
     },
   ],
   'oil-field': [
@@ -130,65 +130,65 @@ export const productData = {
       title: 'Bentonite',
       description:
         'A clay mineral used in drilling muds to improve viscosity and filtration control.',
-      image: '../public/productImage25.JPG',
+      image: '/productImage25.JPG',
     },
     {
       category: 'Oil Field',
       title: 'Biocides',
       description: 'Chemicals used to control bacterial growth in drilling and production fluids.',
-      image: '../public/productImage26.JPG',
+      image: '/productImage26.JPG',
     },
     {
       category: 'Oil Field',
       title: 'Bentonite Extender',
       description:
         'A chemical additive that enhances the performance of bentonite in drilling fluids.',
-      image: '../public/productImage27.JPG',
+      image: '/productImage27.JPG',
     },
     {
       category: 'Oil Field',
       title: 'Calcium Carbonate',
       description: 'A weighting agent used in drilling fluids to control formation pressures.',
-      image: '../public/productImage28.JPG',
+      image: '/productImage28.JPG',
     },
     {
       category: 'Oil Field',
       title: 'Cellulosic Thickener',
       description: 'A viscosity enhancer that stabilizes drilling fluids',
-      image: '../public/productImage29.JPG',
+      image: '/productImage29.JPG',
     },
     {
       category: 'Oil Field',
       title: 'Defoamers',
       description:
         'Additives used to reduce foam formation in drilling muds and production processes.',
-      image: '../public/productImage30.JPG',
+      image: '/productImage30.JPG',
     },
     {
       category: 'Oil Field',
       title: 'Drilling Detergents',
       description: 'Surfactants used to reduce drilling torque and improve mud flow.',
-      image: '../public/productImage31.JPG',
+      image: '/productImage31.JPG',
     },
     {
       category: 'Oil Field',
       title: 'Guar Gum',
       description: 'A natural polymer used to enhance viscosity in drilling fluids.',
-      image: '../public/productImage32.JPG',
+      image: '/productImage32.JPG',
     },
     {
       category: 'Oil Field',
       title: 'Lubricants',
       description:
         'Reduce friction in drilling operations to improve efficiency and protect equipment.',
-      image: '../public/productImage33.JPG',
+      image: '/productImage33.JPG',
     },
     {
       category: 'Oil Field',
       title: 'Methanol',
       description:
         'A hydrate inhibitor used in natural gas production to prevent pipeline blockages.',
-      image: '../public/productImage34.JPG',
+      image: '/productImage34.JPG',
     },
   ],
   'water-treatment': [
@@ -196,35 +196,35 @@ export const productData = {
       category: 'Water Treatment',
       title: 'Aluminium Sulphate',
       description: 'A coagulant used in water purification to remove impurities and clarify water.',
-      image: '../public/productImage35.JPG',
+      image: '/productImage35.JPG',
     },
     {
       category: 'Water Treatment',
       title: 'Calcium Hypochlorite',
       description:
         'A disinfectant and bleaching agent used in water treatment to kill bacteria and sanitize..',
-      image: '../public/productImage36.JPG',
+      image: '/productImage36.JPG',
     },
     {
       category: 'Water Treatment',
       title: 'Ferric Chloride',
       description:
         'A coagulating agent used in water treatment to remove suspended solids and control turbidity.',
-      image: '../public/productImage12.JPG',
+      image: '/productImage12.JPG',
     },
     {
       category: 'Water Treatment',
       title: 'Ferrous Sulphate',
       description:
         'A reducing agent used in water treatment for phosphate removal and coagulation.',
-      image: '../public/productImage13.JPG',
+      image: '/productImage13.JPG',
     },
     {
       category: 'Water Treatment',
       title: 'Hydrated Lime',
       description:
         'Used to adjust water pH and remove impurities, including heavy metals and organic matter',
-      image: '../public/productImage14.JPG',
+      image: '/productImage14.JPG',
     },
   ],
 }
